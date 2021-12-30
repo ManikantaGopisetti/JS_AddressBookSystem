3ad6162c96f0b296908dcc8aa080fa89f1c7cc46
ManikantaGopisetti <manikantagopisetti2@gmail.com>
2021-12-30 19:17:34 +0530

    Added: Ability to add contacts into array

--- AddressBook.js
+++ AddressBook.js
@@ -23,20 +23,18 @@ class Contact {
 detailValid = (pattern, detail) => {
     let result = pattern.test(detail);
     if (result == false) {
-        throw detail + ' is not valid';
+        throw detail + ' is not valid enter valid deatils';
     }
     return result;
 }
 
 validContact = (...params) => {
-    if (params[0] == true && params[1] == true && params[2] == true && params[4] == true 
+    if (params[0] == true && params[1] == true && params[2] == true && params[4] == true
         && params[5] == true && params[6] == true && params[7] == true) {
         return true;
     }
     return false;
 }
-
-console.log("\n-----Welcome to Address Book-----\n");
 
 let firstNamePattern = /^[A-Z][a-z]{2,}$/;
 let lastNamePattern = /^[A-Z][a-z]{2,}$/;
@@ -47,21 +45,32 @@ let zipPattern = /^[\d]{3}[\s][\d]{3}$/;
 let phoneNumberPattern = /^[\d]{2}[\s]{1}[\d]{10}$/;
 let emailPattern = /^([-+a-zA-z0-9]+([.][a-zA-z0-9]+)*)@([\w]+[.][a-zA-z]{2,}([.][a-zA-Z]{2,})?)$/;
 
-let firstName = 'Mani';
-let lastName = 'Gopisetti';
-let address = 'India';
-let city = 'Ravulapalem';
-let state = 'Andhra';
-let zip = '533 236';
-let phoneNumber = '91 9876543210';
-let email = 'mani@gmail.com';
+addContact = () => {
+    let firstName = 'Mani';
+    let lastName = 'Gopisetti';
+    let address = 'India';
+    let city = 'Ravulapalem';
+    let state = 'Andhra';
+    let zip = '533 236';
+    let phoneNumber = '91 9876543210';
+    let email = 'mani@gmail.com';
+    try {
+        if (validContact(detailValid(firstNamePattern, firstName), detailValid(lastNamePattern, lastName),
+            detailValid(addressPattern, address), detailValid(cityPattern, city), detailValid(statePattern, state),
+            detailValid(zipPattern, zip), detailValid(phoneNumberPattern, phoneNumber), detailValid(emailPattern, email))) {
 
-let contact1;
-if (validContact(detailValid(firstNamePattern, firstName), detailValid(lastNamePattern, lastName),
-    detailValid(addressPattern, address), detailValid(cityPattern, city), detailValid(statePattern, state),
-    detailValid(zipPattern, zip), detailValid(phoneNumberPattern, phoneNumber), detailValid(emailPattern, email))) {
+            let contact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+            contactArr.push(contact);
 
-    contact1 = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
-
+        }
+    }
+    catch (e) {
+        console.error(e + "\n");
+    }
 }
-console.log(contact1);
+
+console.log("\n-----Welcome to Address Book-----\n");
+
+let contactArr = new Array();
+addContact();
+console.log(contactArr);

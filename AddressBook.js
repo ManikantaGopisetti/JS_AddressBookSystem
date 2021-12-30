e51f2c2aa4e811a007a014f357aadc38554f2749
ManikantaGopisetti <manikantagopisetti2@gmail.com>
2021-12-30 18:53:05 +0530

    refractor to correct indentation function to check validity using regex patterns

--- AddressBook.js
+++ AddressBook.js
@@ -1,4 +1,4 @@
-class Contact{
+class Contact {
     firstName;
     lastName;
     address;
@@ -8,7 +8,7 @@ class Contact{
     phoneNumber;
     email;
 
-    constructor(...params){
+    constructor(...params) {
         this.firstName = params[0];
         this.lastName = params[1];
         this.address = params[2];
@@ -20,15 +20,17 @@ class Contact{
     }
 }
 
-detailValid=(pattern,detail)=>{
-    let result=pattern.test(detail);
-    if(result==false){
-        throw detail+ ' is not valid';
+detailValid = (pattern, detail) => {
+    let result = pattern.test(detail);
+    if (result == false) {
+        throw detail + ' is not valid';
     }
     return result;
 }
-validContact=(...params)=>{
-    if(params[0]==true&&params[1]==true&&params[2]==true&&params[4]==true&&params[5]==true&&params[6]==true&&params[7]==true){
+
+validContact = (...params) => {
+    if (params[0] == true && params[1] == true && params[2] == true && params[4] == true 
+        && params[5] == true && params[6] == true && params[7] == true) {
         return true;
     }
     return false;
@@ -36,31 +38,30 @@ validContact=(...params)=>{
 
 console.log("\n-----Welcome to Address Book-----\n");
 
-let firstNamePattern=/^[A-Z][a-z]{2,}$/;
-let lastNamePattern=/^[A-Z][a-z]{2,}$/;
-let addressPattern=/^[A-Za-z]{4,}$/;
-let cityPattern=/^[A-Za-z]{4,}$/;
-let statePattern=/^[A-Za-z]{4,}$/;
-let zipPattern=/^[\d]{3}[\s][\d]{3}$/;
+let firstNamePattern = /^[A-Z][a-z]{2,}$/;
+let lastNamePattern = /^[A-Z][a-z]{2,}$/;
+let addressPattern = /^[A-Za-z]{4,}$/;
+let cityPattern = /^[A-Za-z]{4,}$/;
+let statePattern = /^[A-Za-z]{4,}$/;
+let zipPattern = /^[\d]{3}[\s][\d]{3}$/;
 let phoneNumberPattern = /^[\d]{2}[\s]{1}[\d]{10}$/;
-let emailPattern=/^([-+a-zA-z0-9]+([.][a-zA-z0-9]+)*)@([\w]+[.][a-zA-z]{2,}([.][a-zA-Z]{2,})?)$/;
+let emailPattern = /^([-+a-zA-z0-9]+([.][a-zA-z0-9]+)*)@([\w]+[.][a-zA-z]{2,}([.][a-zA-Z]{2,})?)$/;
 
-let firstName='Mani';
-let lastName='Gopisetti';
-let address='India';
+let firstName = 'Mani';
+let lastName = 'Gopisetti';
+let address = 'India';
 let city = 'Ravulapalem';
 let state = 'Andhra';
-let zip='533 236';
+let zip = '533 236';
 let phoneNumber = '91 9876543210';
 let email = 'mani@gmail.com';
 
 let contact1;
-if(validContact(detailValid(firstNamePattern,firstName),detailValid(lastNamePattern,lastName),
-    detailValid(addressPattern,address),detailValid(cityPattern,city),detailValid(statePattern,state),
-    detailValid(zipPattern,zip),detailValid(phoneNumberPattern,phoneNumber),detailValid(emailPattern,email))){
+if (validContact(detailValid(firstNamePattern, firstName), detailValid(lastNamePattern, lastName),
+    detailValid(addressPattern, address), detailValid(cityPattern, city), detailValid(statePattern, state),
+    detailValid(zipPattern, zip), detailValid(phoneNumberPattern, phoneNumber), detailValid(emailPattern, email))) {
 
-      contact1= new Contact(firstName,lastName,address,city,state,zip,phoneNumber,email);
-       
+    contact1 = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+
 }
 console.log(contact1);
-
